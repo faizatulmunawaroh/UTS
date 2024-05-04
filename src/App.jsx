fc9dea434c6ea5fa54e9c15e0f22d26087d86026
faizatulmunawaroh <faizatulmunawaroh260@gmail.com>
2024-05-04 20:01:55 +0700

    commit again

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import Home from './screens/Home';
 import profil from './screens/profil';
@@ -15,9 +16,27 @@ const Tab = createBottomTabNavigator();
 function MyTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Profil" component={profil} />
-      <Tab.Screen name="Pengaturan" component={Pengaturan} />
+      <Tab.Screen name="Beranda" component={Home} 
+      options={{
+          tabBarLabel: 'Beranda',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}/>
+      <Tab.Screen name="Profil" component={profil} 
+      options={{
+          tabBarLabel: 'Profil',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
+        }}/>
+      <Tab.Screen name="Pengaturan" component={Pengaturan} 
+      options={{
+          tabBarLabel: 'Pengaturan',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="cog" color={color} size={26} />
+          ),
+        }}/>
     </Tab.Navigator>
   );
 }
@@ -25,7 +44,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={MyTabs} />
+        <Stack.Screen name="Home" component={MyTabs} options={{headerShown:false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/profil.jsx
+++ src/screens/profil.jsx
@@ -1,10 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet,View } from 'react-native'
 import React from 'react'
 
 const profil = () => {
   return (
     <View>
-      <Text>profil</Text>
     </View>
   )
 }
